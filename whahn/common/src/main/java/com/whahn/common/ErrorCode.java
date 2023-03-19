afa830705a43ed108069688170d75fe21dd44269
wonhwiahn <myroom9@naver.com>
2023-03-19 11:52:36 +0900

    하... 눈물난다 진짜

--- whahn/common/src/main/java/com/whahn/common/ErrorCode.java
+++ whahn/common/src/main/java/com/whahn/common/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     INTERNAL_EXCEPTION(500, "50000000", "서비스 내부에서 알 수 없는 에러가 발생하였습니다."),
+    NOT_SUPPORT_API(405, "40500000", "지원하지않는 API입니다."),
     NOT_SUPPORT_API_CORPORATION(400, "40000000", "API를 지원하지 않는 회사입니다."),
     NOT_SUPPORT_SERVICE_CORPORATION(400, "40000001", "블로그 조회 서비스를 지원하지 않는 회사입니다."),
 
